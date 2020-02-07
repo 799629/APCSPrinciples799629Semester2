841813edd2c5739feee01b8153c67f46c8d5e403
799629 <799629@seq.org>
2020-02-07 08:41:04 -0800

    needs organizing

--- Semester 2/Project - Big Data/sketch.js
+++ Semester 2/Project - Big Data/sketch.js
@@ -1,8 +1,10 @@
 //  Kawika Tu
-// 	Date
+// 	2/7/20
 //  This is a comment
-//  The setup function function is call
-ed once when your program begins
+
+var stats
+var players
+
 function setup(){
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
@@ -12,3 +14,33 @@ function setup(){
 
 function draw(){
 }//End Draw
+
+function createPlayerSelectionList() {
+  playerSel = createSelect(true); playerSel.position((windowWidth-width)/2 + 270, (windowHeight-height)/2 + 40);
+ // locate at 270,40 in canvas coordinates playerSel.size(150,headerHeight-50);
+}
+
+// abstract the UI control away, put the chosen player(s) in the array chosenPlayers function getSelectedPlayers() {
+chosenPlayers = [];
+for (var i = 0; i<playerSel.elt.selectedOptions.length; i++) {
+chosenPlayers.push(playerSel.elt.selectedOptions[i].value); } }
+
+// find the stats for the chosen player in the stats table. result is an array of table rows, one for each year the player was in the league
+function loadPlayerStats(player) {
+// column 2 has the player's name in the stats table
+statsArray = stats.findRows(player, 2);
+  if (statsArray.length === 0) {
+// try adding an '*'
+    statsArray = stats.findRows(player+"*", 2); }
+}
+
+// collect stats into arrays for generic approach to graphing
+function aggregateStats(player, stat) {
+  results = [];
+for (var i =0; i<statsArray.length; i++) {
+
+  for (var i=0; i<statsToPlot.length; i++) {
+  lerpRatio = i/(statsToPlot.length-1); colorToUse = lerpColor(colorStart, colorEnd, lerpRatio);
+  ...
+
+y = map(values[i], smallest, largest, 0, drawAreaHeight); 
